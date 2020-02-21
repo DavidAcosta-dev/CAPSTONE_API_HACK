6f0406e238649cbbe44c5486004debd169728db5
David Acosta <acosta816@gmail.com>
2020-02-20 17:41:05 -0800

    created ghostKey inside of ghost.js file and added to .gitignore

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 
 
 STORE = {
-    apiKey: '6cc43a33831a4c2fac29d444bca22ecb',
+    apiKey: ghostKey,
     baseUrl: 'https://api.spoonacular.com/recipes/findByIngredients?number=5&ranking=1&',
     list: []
 }
